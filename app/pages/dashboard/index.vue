10e83bd05817a565652314c4db3dfbb5e10a0af9
Redomeire <redomeire2@gmail.com>
2025-08-13 13:14:39 +0700

    feat: create dashboard layout

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,31 +1,62 @@
 <template>
   <div class="dashboard">
-    <h1>Dashboard</h1>
-    <p>Welcome to your dashboard!</p>
-    <p>This is a placeholder for your dashboard content.</p>
-    <button @click="handleLogout">
-      Logout
-    </button>
+    <ul class="grid grid-cols-4 gap-3">
+      <li
+        v-for="boxItem in boxItems"
+        :key="boxItem.title"
+        :class="containerClasses(boxItem.color)"
+      >
+        <h2 class="text-heading-6">
+          {{ boxItem.title }}
+        </h2>
+        <p class="text-heading-4 font-semibold">
+          {{ boxItem.value }}
+        </p>
+      </li>
+    </ul>
+    <div class="grid grid-cols-5 gap-5 mt-5">
+      <div class="rounded-lg shadow-xl col-span-2 min-h-48" />
+      <div class="rounded-lg shadow-xl col-span-3 min-h-48" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// context data
-const { $useCases } = useNuxtApp()
-const { clear } = useUserSession()
-const router = useRouter()
+definePageMeta({
+  layout: 'dashboard'
+})
+const boxItems = [
+  {
+    title: 'Total belum dibayar',
+    value: 'Rp. 1.000.000',
+    icon: 'i-material-symbols-receipt-long-outline',
+    color: 'bg-error-200'
+  },
+  {
+    title: 'Tagihan mendatang',
+    value: '3',
+    icon: 'i-material-symbols-receipt-long-outline',
+    color: 'bg-primary-200'
+  },
+  {
+    title: 'Total tagihan',
+    value: '6',
+    icon: 'i-material-symbols-person-outline',
+    color: 'bg-success-200'
+  },
+  {
+    title: 'Terlambat bayar',
+    value: '1',
+    icon: 'i-material-symbols-payment-outline',
+    color: 'bg-warning-200'
+  }
+]
 
-// api fetch and function
-const handleLogout = async () => {
-  Promise.all([
-    $useCases.auth.logout.execute(),
-    clear()
-  ]).catch((error) => {
-    console.error('Error during logout:', error)
-  }).finally(() => {
-    setTimeout(() => {
-      router.replace('/auth/login')
-    }, 1000)
-  })
+const containerClasses = (color: string) => {
+  return cn(
+    'flex flex-col items-center justify-center',
+    `rounded-lg p-4`,
+    color
+  )
 }
 </script>
